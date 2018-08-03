1556d86813070820816c8f70b8f06fee863f8f2b
Тощев Дмитрий Владимирович <toshchevdv@site-master.org>
2018-08-03 18:14:46 +0300

    last commit

--- app/js/common.js
+++ app/js/common.js
@@ -17,6 +17,11 @@ $(function() {
 		$(this).toggleClass('boilerList__checkbox_active');
 	});
 
+	// Функция разделителя для числа
+	function numberSeparator(numberToSep){
+		return numberToSep.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+	}
+
 	/*Изменяет значение итоговой суммы секции в соответствии с атрибутром data-total-price*/
 	function totalPriceSec(){
 		let totalPriceSecValue = $('.cbTotal__total').attr('data-total-price');
@@ -34,14 +39,60 @@ $(function() {
 		totalPriceSec();
 	}
 
+	// Показ скрытых моделей в блоке выбора бойлера
+	const modelsCount = $('.modelsList').children('.boilerList__block');
+	if (modelsCount.length > 4) {
+		$('#showMore').css("display", "block");
+		for (i=4; i<modelsCount.length; i++) {
+			let obj = $(modelsCount[i]);
+			modelsCount[i].style.opacity = "0";
+			obj.css('height', '0');
+			obj.css('marginBottom', '0');
+		}
+	}
+	$('#showMore').on('click', function(){
+		let modelsHiddenCount = $('.modelsList').children('.boilerList__block[style~="opacity: 0;"]');
+		for (i=0; i<4; i++) {
+			modelsHiddenCount[i].style.opacity = "1";
+			$(modelsHiddenCount[i]).css('height', 'auto');
+			$(modelsHiddenCount[i]).css('marginBottom', '10px');
+		}
+	});
 
-/*
-	const numberSepBefore = $('.thousands-separator').text();
-	console.log(numberSepBefore);
-	const numberSepAfter = numberSepBefore.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
-	console.log(numberSepAfter);
-	$('.thousands-separator').text(numberSepAfter);
-*/
+	// Показ скрытых моделей в блоке выбора котла
+	const modelsCount2 = $('.modelsList2').children('.boilerList__block');
+	if (modelsCount2.length > 4) {
+		$('#showMore2').css("display", "block");
+		for (i=4; i<modelsCount2.length; i++) {
+			let obj = $(modelsCount2[i]);
+			modelsCount2[i].style.opacity = "0";
+			obj.css('height', '0');
+			obj.css('marginBottom', '0');
+		}
+	}
+	$('#showMore2').on('click', function(){
+		let modelsHiddenCount2 = $('.modelsList2').children('.boilerList__block[style~="opacity: 0;"]');
+		for (i=0; i<4; i++) {
+			modelsHiddenCount2[i].style.opacity = "1";
+			$(modelsHiddenCount2[i]).css('height', 'auto');
+			$(modelsHiddenCount2[i]).css('marginBottom', '10px');
+		}
+	});
+
+
+	$('#button22').on('click', function(){
+		$('.inputValue').val(200000);
+		$('.inputValue').trigger('change');
+	});
+
+	$('.inputValue').on('change', function() {
+		$(this).parent().children('.cbSeparately__value').text(numberSeparator($(this).val()));
+		$('.cbTotal__total').text($(this).parent('.costBlock__total').siblings('.costBlock__separately').find('.cbSeparately__value').text())
+	});
+
+
+
+
 
 
 });
